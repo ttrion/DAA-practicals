2e825db7ebb3a854c35badc4d1fa9a10107d1b7b
ttrion <23bcs13761@cuchd.in>
2025-08-24 22:25:29 +0530

    Update DAA Expt. 3.java

--- Experiment 3/DAA Expt. 3.java
+++ Experiment 3/DAA Expt. 3.java
@@ -1,4 +1,23 @@
-/*
-Aim : Code to find frequency of elements in a given array in ONo time complexity.
-*/
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> countFreq(int[] arr) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int num : arr) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            ArrayList<Integer> pair = new ArrayList<>();
+            pair.add(entry.getKey());
+            pair.add(entry.getValue());
+            result.add(pair);
+        }
+        
+        return result;
+    }
+}
+
 
